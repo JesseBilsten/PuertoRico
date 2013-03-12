353ba08768bd01824f6333f3f618677da0f42073
Jesse Bilsten <jesse@bilsten.net>
2013-03-11 21:24:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuertoRico::Application.config.secret_token = 'ac6daf06aab5155ccdb4bd2dd42f25a926b21c8c40dc7d29fba95603938f2e10bec2eedec48aef44c4c15f0c4619194df8e58550a46efde67f0b92962ece0936'
